59ecb65e3dc714eb4e6532abefe8101674dffb2e
Laurent Poulin <laurent.poulin420@gmail.com>
2016-02-10 11:14:06 +0100

    sauvegarde (Q6 TP2)

--- Tab/app/src/main/java/ovh/poulin/tab/Modele.java
+++ Tab/app/src/main/java/ovh/poulin/tab/Modele.java
@@ -1,7 +1,56 @@
 package ovh.poulin.tab;
 
+import android.content.Context;
+import android.os.Bundle;
+
 /**
  * Created by poulinl on 10/02/16.
  */
-public class Modele {
+public class Modele{
+    //private Map<Integer,Integer> tablets = new ArrayMap<Integer,Integer>();
+    private String[] students;
+    private String[] tabletsArr;
+    private int[] tablets;
+
+    public Modele(Context c){
+        this.tabletsArr = c.getResources().getStringArray(R.array.tablets);
+        this.students = c.getResources().getStringArray(R.array.students);
+        this.tablets = new int[tabletsArr.length];
+        for(int i = 0; i < tabletsArr.length ; i++){
+            this.tablets[i] = 0;
+        }
+    }
+
+    public Modele(Bundle inState){
+        tablets = inState.getIntArray("TABLETS_ARRAY");
+        students = inState.getStringArray("TABLETS_NAMES");
+        students = inState.getStringArray("STUDENTS_NAMES");
+
+    }
+
+    public int getCount(){
+        return tablets.length;
+    }
+
+    public String getStudent(int i){
+        return students[tablets[i]];
+    }
+
+    public String getTerminal(int i){
+        return tabletsArr[i];
+    }
+
+    public boolean setStudent(int i,int e){
+        if(tablets[i] != 0){
+            return false;
+        }
+        tablets[i] = e;
+        return true;
+    }
+
+    public void save(Bundle OutState){
+        OutState.putIntArray("TABLETS_ARRAY", tablets);
+        OutState.putStringArray("TABLETS_NAMES", tabletsArr);
+        OutState.putStringArray("STUDENTS_NAMES",students);
+    }
 }
