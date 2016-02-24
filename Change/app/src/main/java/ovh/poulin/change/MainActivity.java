6aba6cfaa23aefd38cd681bd5211779190ffac5c
Laurent Poulin <laurent.poulin420@gmail.com>
2016-02-24 11:17:31 +0100

    Ajout

--- Change/app/src/main/java/ovh/poulin/change/MainActivity.java
+++ Change/app/src/main/java/ovh/poulin/change/MainActivity.java
@@ -4,12 +4,46 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    TextView tvResult; Spinner spEntry, spOut; EditText etEntry; Button btConvert;
+
+    AdapterView.OnItemSelectedListener listener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            if(view == findViewById(R.id.spOut)){
+
+            }else{
+
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        tvResult = (TextView) findViewById(R.id.tvresult);
+        spEntry = (Spinner) findViewById(R.id.spEntry);
+        spOut = (Spinner) findViewById(R.id.spOut);
+        etEntry = (EditText) findViewById(R.id.etEntry);
+        btConvert = (Button) findViewById(R.id.convert);
+        ArrayAdapter spaEntry = ArrayAdapter.createFromResource(this, R.array.currencies, android.R.layout.simple_spinner_item);
+        ArrayAdapter spaOut = ArrayAdapter.createFromResource(this, R.array.currencies, android.R.layout.simple_spinner_item);
+        spOut.setAdapter(spaOut);
+        spOut.setOnItemSelectedListener(listener);
+        spEntry.setAdapter(spaEntry);
         setContentView(R.layout.activity_main);
     }
 
@@ -33,5 +67,15 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
